d2c650d8bd5bc47fe96b960a6b9e511743992e18
Lil-boss <rshaon09@gmail.com>
2022-07-12 21:28:03 +0600

    update

--- output/funSignature.js
+++ output/funSignature.js
@@ -3,3 +3,15 @@ let add;
 add = (a, b) => {
     return a + b;
 };
+//advanced
+let calculation;
+calculation = (a, b, c) => {
+    if (c === "add") {
+        return a + b;
+    }
+    else {
+        return a - b;
+    }
+};
+let result = calculation(10, 5, "add");
+console.log(result);
--- src/funSignature.ts
+++ src/funSignature.ts
@@ -3,3 +3,17 @@ let add: (x: number, y: number) => number;
 add=(a:number, b:number) => {
     return a+b;
 }
+
+
+//advanced
+let calculation : (x: number, y: number, z:string) => number;
+
+calculation = (a:number, b:number, c:string) => {
+    if(c==="add"){
+        return a + b;
+    }else{
+        return a -b;
+    }
+}
+let result = calculation(10, 5, "add");
+console.log(result);
